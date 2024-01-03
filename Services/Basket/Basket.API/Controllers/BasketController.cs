1fcc515299cdb8d0cf99ae8d0bf98a104cf77462
Felipe Chiarotti <felipechi97@gmail.com>
2024-01-03 17:32:01 -0300

    #3 Completed Basket Project and Created the Discount microservice

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    public class BasketController : APIController
+    {
+        private readonly IMediator _mediator;
+
+        public BasketController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
+        {
+            var query = new GetBasketByUserNameQuery(userName);
+            var basket = await _mediator.Send(query);
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult<ShoppingCartResponse>> CreateBasket([FromBody] CreateShoppingCartCommand command)
+        {
+            var basket = await _mediator.Send(command);
+            return Ok(basket);
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public async Task<ActionResult<ShoppingCartResponse>> DeleteBasket([FromBody] DeleteBasketByUserNameCommand command)
+        {
+            var basket = await _mediator.Send(command);
+            return Ok(basket);
+        }
+    }
+}
---
+++ Services/Basket/Basket.Application/Responses/ShoppingCartResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.Application.Responses
+{
+    public class ShoppingCartResponse
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItemResponse> Items { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = Items.Sum(x => x.Quantity * x.Price);
+                return totalPrice;
+            }
+        }
+
+        public ShoppingCartResponse() { }
+
+        public ShoppingCartResponse(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
